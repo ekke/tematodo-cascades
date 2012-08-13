5332f3750b7b7c2c9cc59e274a421a12513cdb7f
ekkes-corner <ekke@ekkes-corner.org>
2012-08-13 21:44:33 +0200

    initial project created
    based on template NavigationPane
    
    Signed-off-by: ekkes-corner <ekke@ekkes-corner.org>

---
+++ translations/tematodo.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>DetailPage</name>
+    <message>
+        <location filename="../assets/DetailPage.qml" line="9"/>
+        <source>Break</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/DetailPage.qml" line="22"/>
+        <source>Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/DetailPage.qml" line="41"/>
+        <source>Picture description</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="17"/>
+        <source>Show detail</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
